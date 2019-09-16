253ebf3da761b1c1cafea1c7d40cc8ae94ea9608
Kylin <yhlcode@gmail.com>
2019-09-16 22:22:47 +0800

    taskConfigCode

--- src/views/moments/AuditStatus.vue
+++ src/views/moments/AuditStatus.vue
@@ -82,7 +82,7 @@ export default {
   //方法集合
   methods: {
     fnEnsure() {
-      const type = taskConfigCode == "SharePoster_58" ? "58 " : "YG";
+      const type = this.taskConfigCode == "SharePoster_58" ? "58 " : "YG";
       if (this.data.status != 1) {
         this.$router.push(`/moments/${type}`);
       } else {
