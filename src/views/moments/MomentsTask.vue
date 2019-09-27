ec600dd503c476d8262a072772f1ab480630c482
Kylin <yhlcode@gmail.com>
2019-09-27 11:44:44 +0800

    文案

--- src/views/moments/MomentsTask.vue
+++ src/views/moments/MomentsTask.vue
@@ -72,7 +72,7 @@
             <span></span>P图或上传重复截图作弊者审核将会被拒绝
           </div>
           <div class="item_text">
-            <span></span>本任务每天可参与一次，每天一0点刷新
+            <span></span>本任务每天可参与一次，每天0点刷新
           </div>
         </div>
       </div>
